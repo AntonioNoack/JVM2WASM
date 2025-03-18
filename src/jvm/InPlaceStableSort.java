ca5b59133e2d114afe4460dcf723e2f2ec8290b4
Antonio Noack <antonio-noack@gmx.de>
2025-03-18 20:30:06 +0100

    Making sorting more readable, and removing recursion in merge()

--- src/jvm/InPlaceStableSort.java
+++ src/jvm/InPlaceStableSort.java
@@ -2,10 +2,6 @@ package jvm;
 
 import java.util.Comparator;
 
-import static jvm.JVM32.*;
-import static jvm.JVMShared.read32;
-import static jvm.JVMShared.write32;
-
 // https://www.codeproject.com/Articles/26048/Fastest-In-Place-Stable-Sort
 // http://thomas.baudel.name/Visualisation/VisuTri/inplacestablesort.html
 
@@ -14,135 +10,52 @@ import static jvm.JVMShared.write32;
  */
 class InPlaceStableSort {
 
-    private static <V> int lower(int from, int to, V val, V[] array, Comparator<V> c) {
-        int len = to - from, half;
-        while (len > 0) {
-            half = len >> 1;
-            final int mid = from + half;
-            if (c.compare(array[mid], val) < 0) {
-                from = mid + 1;
-                len = len - half - 1;
-            } else len = half;
+    private static <V> void slowSort(int from, int to, V[] array, Comparator<V> c) {
+        for (int i = from, to1 = to - 1; i < to1; i++) {
+            for (int j = i + 1; j < to; j++) {
+                V vi = array[i];
+                V vj = array[j];
+                if (c.compare(vi, vj) > 0) {
+                    array[i] = vj;
+                    array[j] = vi;
+                }
+            }
         }
-        return from;
     }
 
-    private static <V> int upper(int from, int to, V val, V[] array, Comparator<V> c) {
-        int len = to - from, half;
-        while (len > 0) {
-            half = len >> 1;
-            final int mid = from + half;
-            if (c.compare(val, array[mid]) < 0) {
-                len = half;
+    private static <V> void merge(int from, int pivot, int to, V[] arr, Comparator<V> c) {
+        while (from < pivot && pivot < to) {
+            if (c.compare(arr[from], arr[pivot]) <= 0) {
+                from++;
             } else {
-                from = mid + 1;
-                len = len - half - 1;
+                V tmp = arr[pivot];
+                int k = pivot;
+                while (k > from) {
+                    arr[k] = arr[k - 1];
+                    k--;
+                }
+                arr[from] = tmp;
+                from++;
+                pivot++;
             }
         }
-        return from;
-    }
-
-    private static <V> void insertSort(int from, int to, V[] array, Comparator<V> c) {
-        // check bounds first, and then we can use unsafe element access
-        if (from < 0 || c == null || array.length < to) {
-            throw new IllegalArgumentException();
-        }
-        // it would be nice if we could resolve the method call for c right here
-        int addr = getAddr(array) + arrayOverhead;
-        int fromAddr = addr + (from << 2);
-        int toAddr = addr + (to << 2);
-        for (int i = fromAddr + 4; unsignedLessThan(i, toAddr); i += 4) {
-            insertSortInner(fromAddr, i, c);
-        }
-    }
-
-    // it would be nice if we had stable sorting with O(n log n)
-    private static <V> void insertSortInner(int lowerBound, int upperBound, Comparator<V> c) {
-        for (int j = upperBound; unsignedGreaterThan(j, lowerBound); ) {
-            int k = j - 4;
-            int ai = read32(j);
-            int bi = read32(k);
-            final V a = ptrTo(ai);
-            final V b = ptrTo(bi);
-            if (c.compare(a, b) < 0) {
-                write32(k, ai);
-                write32(j, bi);
-                j = k;
-            } else break;
-        }
-    }
-
-    private static int gcd(int m, int n) {
-        while (n != 0) {
-            final int t = m % n;
-            m = n;
-            n = t;
-        }
-        return m;
-    }
-
-    private static <V> void rotate(int from, int mid, int to, V[] array) {
-        if (from == mid || mid == to) return;
-        int n = gcd(to - from, mid - from);
-        while (n-- != 0) {
-            V val = array[from + n];
-            int shift = mid - from;
-            int p1 = from + n, p2 = from + n + shift;
-            while (p2 != from + n) {
-                array[p1] = array[p2];
-                p1 = p2;
-                if (to - p2 > shift) p2 += shift;
-                else p2 = from + (shift - (to - p2));
-            }
-            array[p1] = val;
-        }
-    }
-
-    private static <V> void merge(int from, int pivot, int to, int len1, int len2, V[] array, Comparator<V> c) {
-        if (len1 == 0 || len2 == 0) return;
-        if (len1 + len2 == 2) {
-            V p = array[pivot];
-            V f = array[from];
-            if (c.compare(p, f) < 0) {
-                array[pivot] = f;
-                array[from] = p;
-            }
-            return;
-        }
-        int cut1, cut2;
-        int len11, len22;
-        if (len1 > len2) {
-            len11 = len1 >> 1;
-            cut1 = from + len11;
-            cut2 = lower(pivot, to, array[cut1], array, c);
-            len22 = cut2 - pivot;
-        } else {
-            len22 = len2 >> 1;
-            cut2 = pivot + len22;
-            cut1 = upper(from, pivot, array[cut2], array, c);
-            len11 = cut1 - from;
-        }
-        rotate(cut1, pivot, cut2, array);
-        final int new_mid = cut1 + len22;
-        merge(from, cut1, new_mid, len11, len22, array, c);
-        merge(new_mid, cut2, to, len1 - len11, len2 - len22, array, c);
     }
 
     public static <V> void sort(int from, int to, V[] o, Comparator<V> c) {
         if (from > to) throw new IllegalArgumentException();
         if (to - from < 12) {
-            insertSort(from, to, o, c);
+            slowSort(from, to, o, c);
         } else {
             final int middle = (from + to) >>> 1;
             sort(from, middle, o, c);
             sort(middle, to, o, c);
-            merge(from, middle, to, middle - from, to - middle, o, c);
+            merge(from, middle, to, o, c);
         }
     }
 
     public static void main(String[] args) {
         // works and is stable, confirmed :)
-        Object[] array = {5, 4, 3, 3, 2, 1};
+        Object[] array = {5, 4, 17, 156, 132, 3, 0, 3, 2, 1};
         sort(0, array.length, array, Comparator.comparingInt(o -> (int) o));
         for (int i = 0; i < array.length; i++) {
             System.out.println(i + ":" + array[i]);
